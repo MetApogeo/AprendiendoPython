9123e522abc39ffa36b0c4438f1f2888c707f17d
MetApogeo <139805986+MetApogeo@users.noreply.github.com>
2025-03-01 17:32:11 -0600

    Operadores y Strings

---
+++ 02_operators.py
@@ -0,0 +1,41 @@
+### Operadores
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3) # Siempre da un numero entero redondeando
+print(2 ** 3) # Exponenciación
+
+my_float = 2.5 * 2
+
+print("Hola" + "Python")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+print("Hola " * int(my_float))
+print("Hola " * int(2.5 * 2))
+
+
+### Operadores Comparativos
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 != 4)
+
+print("hola" > "python")
+print("hola" < "python")
+print("aaaa" >= "abaa") # Ordenación alfabetica por ASCII
+print(len("aaaa") <= len("abaa")) #contar caracteres
+print("hola" >= "python")
+print("hola" != "python")
+
+### Operadores Lógicos
+
+print(3 > 4 and "hola" > "python")
+print(3 > 4 or "hola" > "python")
+#print(3 > 4 not "hola" > "python")
